d233e1a40653cad12cdc16851a80f6303a42e573
hackr2009 <omarteam737@gmail.com>
2017-08-14 09:57:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COUNTER=1
 while(true) do
-./TSHAKE.sh
+./run.sh
 curl "https://api.telegram.org/bot[415598331:AAGjgksHhev_nm-Zymj7szD3JrDqbgpN8Dg]/sendmessage" -F "chat_id=67369633" -F "text=#NEWCRASH-#DEVFAEDERapi-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
