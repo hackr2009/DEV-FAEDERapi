88d178a54c51f029067b592ad13fab707a5230a4
hackr2009 <omarteam737@gmail.com>
2017-08-14 17:36:06 +0300

    Delete ruun.sh

--- ruun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COUNTER=1
-while(true) do
-./run.sh
-curl "https://api.telegram.org/bot[415598331:AAGjgksHhev_nm-Zymj7szD3JrDqbgpN8Dg]/sendmessage" -F "chat_id=67369633" -F "text=#NEWCRASH-#FAEDER-Reloaded-${COUNTER}-times"
-let COUNTER=COUNTER+1 
-done
